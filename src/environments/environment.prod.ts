9edb92bb2cd126098b408e1a89a5bb3f17698bb3
Scott Bergler <commandinghands@gmail.com>
2019-11-21 10:05:24 -0800

    add firebase config.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBvLCN7pi_R22hSDVAYpNcB-xOGyVYt0SA",
+    authDomain: "mdp-trainer.firebaseapp.com",
+    databaseURL: "https://mdp-trainer.firebaseio.com",
+    projectId: "mdp-trainer",
+    storageBucket: "mdp-trainer.appspot.com",
+    messagingSenderId: "495140600182",
+    appId: "1:495140600182:web:63a10d3f95af5fbca0a257",
+    measurementId: "G-DDEK41FPCL"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBvLCN7pi_R22hSDVAYpNcB-xOGyVYt0SA",
+    authDomain: "mdp-trainer.firebaseapp.com",
+    databaseURL: "https://mdp-trainer.firebaseio.com",
+    projectId: "mdp-trainer",
+    storageBucket: "mdp-trainer.appspot.com",
+    messagingSenderId: "495140600182",
+    appId: "1:495140600182:web:63a10d3f95af5fbca0a257",
+    measurementId: "G-DDEK41FPCL"
+  }
 };
 
 /*
